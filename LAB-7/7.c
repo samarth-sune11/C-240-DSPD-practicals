94f5797c78a287bd5eff4260cbabff661eb5e3f3
samarth-sune11 <samarthsune11@gmail.com>
2025-11-15 14:10:17 +0530

    Update 7.c

--- LAB-7/7.c
+++ LAB-7/7.c
@@ -1,46 +1,70 @@
-#include<stdio.h>
-int graph[10][10]={
-    {0,0,1,0,1,0,0,0,0,0},
-    {0,1,0,1,0,1,0,0,0,0},
-    {0,0,1,0,0,0,0,0,1,0},
-    {0,1,0,0,0,1,1,0,0,0},
-    {0,0,1,0,1,0,1,1,0,0},
-    {0,0,0,1,0,1,0,0,0,1},
-    {0,0,0,0,1,0,0,0,1,0},
-    {0,0,0,0,0,1,0,1,0,1},
-    {0,0,0,0,0,0,1,0,1,0}
-};
-int visited[10];
-void BFS(int start){
-    int q[20],front=0,rear=0;
-    q[rear++]=start;
-    visited[start]=1;
-    printf("BSF:  ");
-    while(front<rear){
-        int node= q[front++];
-        printf("%d ",node);
-        for(int i=1;i<=9;i++){
-            if(graph[node][i]&& !visited[i]){
-                visited[i]=1;
-                q[rear++]=i;
+#include <stdio.h>
+#define MAX 20
+int adj[MAX][MAX];
+int visited[MAX];
+int stack[MAX], top = -1;
+int queue[MAX], front = -1, rear = -1;
+void DFS(int start, int n) {
+    int i, k;
+    for (i = 0; i < n; i++){
+        visited[i] = 0;
+    }
+    stack[++top] = start;
+    visited[start] = 1;
+    printf("\nDFS Traversal: ");
+    printf("%d ", start + 1);
+    while (top >= 0) {
+        k = stack[top];
+        int found = 0;
+        for (i = n - 1; i >= 0; i--) {
+            if (adj[k][i] == 1 && visited[i] == 0) {
+                stack[++top] = i;
+                visited[i] = 1;
+                printf("%d ", i + 1);
+                found = 1;
+                break;
+            }
+        }
+        if (!found){
+            top--;
+        }
+    }
+}
+void BFS(int start, int n) {
+    int i, k;
+    for (i = 0; i < n; i++){
+        visited[i] = 0;
+    }    
+    front = rear = -1;
+    queue[++rear] = start;
+    visited[start] = 1;
+    printf("\nBFS Traversal: ");
+    printf("%d ", start + 1);
+    while (front != rear) {
+        k = queue[++front];
+        for (i = 0; i < n; i++) {
+            if (adj[k][i] == 1 && visited[i] == 0) {
+                queue[++rear] = i;
+                visited[i] = 1;
+                printf("%d ", i + 1);
             }
         }
     }
 }
-void DFS(int node){
-    printf("%d ",node);
-    visited[node]=1;
-    for(int i=9;i>=1;i--){
-        if(graph[node][i]&& !visited[i]){
-            DFS(i);
+int main() {
+    int n, i, j, start;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    printf("\nEnter adjacency matrix (%d x %d):\n", n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
         }
     }
-}
-int main(){
-    BFS(1);
-    for(int i=1;i<10;i++){
-        visited[i]=0;}
-        printf("\n DFS :");
-        DFS(1);
+    printf("\nEnter start node (1 to %d): ", n);
+    scanf("%d", &start);
+    start = start - 1;
+    DFS(start, n);
+    BFS(start, n);
     return 0;
 }
